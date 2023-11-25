b97c7e9801d163a67cf620801182ef3f80da48b0
ShHaWkK <alexandreuzan9@gmail.com>
2023-11-25 21:15:35 +0100

    Update Alien.java

--- src/entites/Alien.java
+++ src/entites/Alien.java
@@ -5,31 +5,34 @@ import javax.swing.ImageIcon;
 import ressources.Constantes;
 
 public class Alien extends Entite {
-    public Alien(int xPos, int yPos, String strImg1, String strImg2) {
-        super.xPos = xPos;
+
+	public Alien(int xPos, int yPos, String strImg1, String strImg2) {
+		
+		// Initialisation
+		super.xPos = xPos;
 		super.yPos = yPos;
 		super.largeur = Constantes.LARGEUR_ALIEN;
 		super.hauteur = Constantes.HAUTEUR_ALIEN;
+		super.dx = 0;
+		super.dy = 0;
 		super.vivant = true;
-		// Adresse des images de l'alien
+
+		// Address of the images of the alien
+
 		super.strImg1 = strImg1;
 		super.strImg2 = strImg2;
 		super.strImg3 = "/images/alienMeurt.png";
-		// Chargement de l'image de l'alien
+
+		// Loading the image of the alien
 		super.ico = new ImageIcon(getClass().getResource(super.strImg1));
 		super.img = this.ico.getImage();
-    }
-
+	}
     public void choixImage(boolean pos1) {
-        // M�thode qui charge l'image de l'alien selon son �tat et sa position (1 ou 2)
-        if(this.vivant == true) {
-            if(pos1 == true) {super.ico = new ImageIcon(getClass().getResource(strImg1));} 
-             else {super.ico = new ImageIcon(getClass().getResource(strImg2));}
-           }
-           super.img = this.ico.getImage(); // recharge l'image
-       }
-   
-   }
-
-   
-	
+		if(this.vivant == true) {
+		 if(pos1 == true) {super.ico = new ImageIcon(getClass().getResource(strImg1));} 
+		  else {super.ico = new ImageIcon(getClass().getResource(strImg2));}
+		}
+		else {super.ico = new ImageIcon(getClass().getResource(strImg3));}		
+		super.img = this.ico.getImage(); // recharge l'image
+	}
+}
